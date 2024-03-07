91d720bde0a9089f1b9a11d59ec82d988397fba1
Kristian Walseth Krøgenes <kristiankrogenes@gmail.com>
2024-03-07 11:32:58 +0100

    Cleaned.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
 import argparse
-import torch
 import kornia.augmentation as KA 
 
 from dataset import BFSDataset
@@ -9,16 +9,28 @@ from utils.utils import check_parameters_for_training
 
 class CFG():
 
-    def __init__(self, epochs, size, batch_size, timesteps, model_path, save_model):
-        if check_parameters_for_training(epochs, size, batch_size, timesteps, model_path):
+    def __init__(self, epochs, size, batch_size, timesteps, model_path=None, backbone=False, save_model=None, default=False):
+
+        if default:
+            self.start_epoch = 1
+            self.epochs = 1000
+            self.TARGET_SIZE = (128, 128)
+            self.batch_size = 8
+            self.timesteps = 100
+            self.model_path = None
+            self.save_model = save_model
+
+        elif check_parameters_for_training(epochs, size, batch_size, timesteps, model_path, save_model):
+            self.start_epoch = 1 if backbone else int(re.search(r'e(\d+)\.ckpt', model_path).group(1))
             self.epochs = epochs
             self.TARGET_SIZE = (size, size)
             self.batch_size = batch_size
             self.timesteps = timesteps
             self.model_path = model_path
-            self.save_model = False if save_model==None else save_model
+            self.save_model = save_model
+            
         else:
-            raise ValueError("Something went wrong.") 
+            raise ValueError("Something went wrong.")
 
 if __name__ == "__main__":
 
@@ -29,11 +41,13 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', type=int, help="The batch size")
     parser.add_argument('--timesteps', type=int, help="The number of timesteps.")
     parser.add_argument('--model_path', type=str, help="The path of a model to continue training on.")
-    parser.add_argument('--save_model', type=bool, help="Should the model be saved or not.")
+    parser.add_argument('--backbone', type=str, help="Is it a backbone.")
+    parser.add_argument('--save_model', type=str, help="Should the model be saved or not. Write the name of your new model.")
+    parser.add_argument('--default', type=bool, help="Use the deafult config parameters.")
     args = parser.parse_args()
     # ==================================================================================================
 
-    cfg = CFG(args.epochs, args.size, args.batch_size, args.timesteps, args.model_path, args.save_model)
+    cfg = CFG(args.epochs, args.size, args.batch_size, args.timesteps, args.model_path, args.backbone, args.save_model, args.default)
 
     T = [
         KA.Resize(size=cfg.TARGET_SIZE, antialias=True),
@@ -55,10 +69,8 @@ if __name__ == "__main__":
     bfs = BFSDiffusionModel(dataset=train_dataset,
                             batch_size=cfg.batch_size,
                             image_size=cfg.TARGET_SIZE,
+                            schedule="linear",
                             num_timesteps=cfg.timesteps,
                             checkpoint=cfg.model_path)
     
-    bfs.train(epochs=cfg.epochs, save_model=cfg.save_model)
-
-
-
+    bfs.train(start_epoch=cfg.start_epoch, epochs=cfg.epochs, parameterization="eps", save_model=cfg.save_model)
